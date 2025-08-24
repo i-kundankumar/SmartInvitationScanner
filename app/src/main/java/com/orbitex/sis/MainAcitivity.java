737bdcbc65bee326f018d099ecdb6ece30a42c08
Kundan Kumar <kundankumar09187@outlook.com>
2025-08-24 17:13:34 +0530

    chore: add ML Kit OCR, Room DB, and Material UI dependencies

---
+++ app/src/main/java/com/orbitex/sis/MainAcitivity.java
@@ -0,0 +1,12 @@
+package com.orbitex.sis;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
